9f58f11b179b6d876124902939428124b7721760
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-02 01:48:33 +0530

    Update script.py

--- SiestaRobot/script.py
+++ SiestaRobot/script.py
@@ -15,7 +15,7 @@ MIKU_IMG = (
       "https://telegra.ph/file/d9775286123e3f24dd5b8.jpg",
 )
 
-TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
+TEXXT = "<b>Heya</b> {} <b>I am Ready To Slay Demons Again</b>"
 
 MIKU_N_IMG = (
       "https://telegra.ph/file/262cf5bb9780cbb96afb7.jpg",
