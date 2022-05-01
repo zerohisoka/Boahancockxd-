bf11ddf0ce6da555f51b4bd8c446f2dc3d1452e7
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-01 17:48:43 +0530

    Add files via upload

--- SiestaRobot/script.py
+++ SiestaRobot/script.py
@@ -10,11 +10,11 @@ PM_START_TEXT = """*Hey Darling, {} ❤️*
 
 
 MIKU_IMG = (
-      "https://telegra.ph/file/262cf5bb9780cbb96afb7.jpg",
-      "https://telegra.ph/file/2371af37367b3454e9cdf.jpg",
-      "https://telegra.ph/file/b3d283f1040d5a2ce8aac.jpg",
-      "https://telegra.ph/file/d9986b3e88fdadc021279.jpg",
-      "https://telegra.ph/file/262cf5bb9780cbb96afb7.jpg",
+      "https://telegra.ph/file/75aeeccfe817f3c197c3b.jpg",
+      "https://telegra.ph/file/b8a440f1dbbc0c17dabdf.jpg",
+      "https://telegra.ph/file/2a3a55d541ef99ffb9a27.jpg",
+      "https://telegra.ph/file/5f1f7fa3cce1a39a3c53e.jpg",
+      "https://telegra.ph/file/541980d45561114fabc95.jpg",
 )
 
 TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
@@ -28,6 +28,6 @@ MIKU_N_IMG = (
 
 )
 
-PM_PHOTO = "https://telegra.ph/file/a319c4b912f2dfd24f17f.jpg"
+PM_PHOTO = "https://telegra.ph/file/5f1f7fa3cce1a39a3c53e.jpg"
 
 GC = "https://telegra.ph/file/d9986b3e88fdadc021279.jpg"
