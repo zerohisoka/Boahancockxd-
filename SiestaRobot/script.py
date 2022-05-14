56e418b6d768ad08696f11ea80e5cc3ec1573d9d
Kazutora Hanemiya <kiravaibhav1298@gmail.com>
2022-05-14 09:40:04 +0530

    Add files via upload

--- SiestaRobot/script.py
+++ SiestaRobot/script.py
@@ -1,31 +1,7 @@
 MIKU_IMG = (
-      "https://telegra.ph/file/75aeeccfe817f3c197c3b.jpg",
-      "https://telegra.ph/file/b8a440f1dbbc0c17dabdf.jpg",
-      "https://telegra.ph/file/2a3a55d541ef99ffb9a27.jpg",
-      "https://telegra.ph/file/5f1f7fa3cce1a39a3c53e.jpg",
-      "https://telegra.ph/file/541980d45561114fabc95.jpg",
-      "https://telegra.ph/file/9b69b299e7f8950000c0d.jpg",
-      "https://telegra.ph/file/d6678a660e9428eb3a344.jpg",
-      "https://telegra.ph/file/621605161195f35fa2338.jpg",
-      "https://telegra.ph/file/4d5717238aba244dd05aa.jpg",
-      "https://telegra.ph/file/40559e412f8c11602ee2c.jpg",
-      "https://telegra.ph/file/ef33a463ab376844e9283.jpg",
-      "https://telegra.ph/file/7c10f57a55bbd4c17ab57.jpg",
-      "https://telegra.ph/file/3fc54dc88181057592c01.jpg",
-      "https://telegra.ph/file/d9775286123e3f24dd5b8.jpg",
+      "https://telegra.ph/file/8647cb5d144ace608f594.jpg",
+      "https://telegra.ph/file/c5af0026153ea6aae9b7e.jpg",
+      "https://telegra.ph/file/4d800b2b66d1bac76b8d2.jpg",
+      "https://telegra.ph/file/8717e721e8cfe4c8533c8.jpg",
+      "https://telegra.ph/file/28d1961cdea44c936a1fa.jpg",
 )
-
-TEXXT = "<b>Heya</b> {} <b>I am Ready To Slay Demons Again</b>"
-
-MIKU_N_IMG = (
-      "https://telegra.ph/file/32061f23f445405551eb5.jpg",
-      "https://telegra.ph/file/7a7ff8264078c8f18a672.jpg",
-      "https://telegra.ph/file/8bcb143d0b4dc18a7d941.jpg",
-      "https://telegra.ph/file/9cc937263ed996cd87f04.jpg",
-      "https://telegra.ph/file/b03114f145bf39e6ba4b3.jpg",
-
-)
-
-PM_PHOTO = "https://telegra.ph/file/ef33a463ab376844e9283.jpg"
-
-GC = "https://telegra.ph/file/d9986b3e88fdadc021279.jpg"
